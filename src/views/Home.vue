238486d3038f5bcf47b4ee67c3ae0bd3914de56e
Aura Bravo <bravo.aura95@gmail.com>
2020-04-24 21:16:36 -0500

    feat: project preview for home built

---
+++ src/views/Home.vue
@@ -0,0 +1,68 @@
+<style lang="scss" src="../styles/home.scss" scoped></style>
+<template>
+  <div class="home__main-container">
+    <div class="max-bound">
+      <img
+        class="logo"
+        alt="Aura Bravo logo"
+        src="../assets/Recurso 1logo.png"
+      />
+      <section class="section-spacer section-spacer--large">
+        <div class="home__hero-wrapper">
+          <div class="home__hero-info__wrapper">
+            <h1>Aura Bravo.</h1>
+            <p>
+              Hi, my name is Aura Bravo. I'm an Industrial Designer passionate
+              about digital media and interfaces, it's design and development.
+              Here is some of my work.
+            </p>
+          </div>
+          <div class="home__hero-image__wrapper">
+            <div class="home__hero-background-image"></div>
+          </div>
+        </div>
+      </section>
+      <section class="section-spacer section-spacer--large">
+        <div class="home__project-wrapper">
+          <p class="home__project-extra-info">Diseño Web - 2020 - Dayvo Sistemas</p>
+          <div class="home__project-image__wrapper">
+            <div class="home__project-background-image"></div>
+          </div>
+          <div class="home__project-info__wrapper">
+            <h2 class="h1">Yoga Isaac Ambel</h2>
+            <div class="home__project-number__wrapper">
+              <p class="home__project-number">01</p>
+              <div class="home__project-line"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section-spacer section-spacer--large">
+        <div class="home__project-wrapper">
+          <p class="home__project-extra-info">Diseño Web - 2020 - Dayvo Sistemas</p>
+          <div class="home__project-image__wrapper">
+            <div class="home__project-background-image"></div>
+          </div>
+          <div class="home__project-info__wrapper">
+            <h2 class="h1">Yoga Isaac Ambel</h2>
+            <div class="home__project-number__wrapper">
+              <p class="home__project-number">01</p>
+              <div class="home__project-line"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+/* import Project from '../components/Project.vue' */
+//import styles from '../styles/home.scss'
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>

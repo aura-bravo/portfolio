cd0492b1be33771af8f515f4856bec00597da161
Aura Bravo <bravo.aura95@gmail.com>
2020-05-02 19:28:07 -0500

    feat: home project view created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,7 +44,7 @@
             <div class="home__project-background-image"></div>
           </div>
           <div class="home__project-info__wrapper">
-            <h2 class="h1">Yoga Isaac Ambel</h2>
+            <h2 class="h1">Infinite Love</h2>
             <div class="home__project-number__wrapper">
               <p class="home__project-number">01</p>
               <div class="home__project-line"></div>
